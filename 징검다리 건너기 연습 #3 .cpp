ed32a17bd38bc0ae5e7f4658de54ce58bbc0f728
mathos6147 <80505704+mathos6147@users.noreply.github.com>
2021-09-04 23:27:39 +0900

    Create 징검다리 건너기 연습 #3 .cpp

---
+++ 징검다리 건너기 연습 #3 .cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int answer = 0;
+    vector<long long> a;
+    for(int i=0; i<stones.size()-k +1; i++){
+        long long w = *max_element(stones.begin() +i, stones.begin() + i +k );
+        a.push_back(w);
+    }
+    sort(a.begin(), a.end());
+    answer = (int)a[0];
+    return answer;
+}
